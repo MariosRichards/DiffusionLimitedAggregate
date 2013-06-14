cc12ad8be85c891ed5e3787551e9259c98413450
Marios Richards <Marios.Richards@gmail.com>
2013-06-14 10:34:42 +0200

    Project Initiated

---
+++ DLAscript.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+// setup - load resources (seed image)
+
+// place seed image onto canvas
+
+// get array of pixels
+
+// loop
+
+// create new walker at some position (eventually the mouseover position)
+
+
+// brownian walk until either
+// brown = (Math.random()*4)<<0; // 0-3
+// x+= brown
+
+
+// 0 -> -1, 1 -> 0, 2 -> 1, 3->0
+
+//   brown & 0x1
+// fun little problem!
+
+
+
+
+// have I hit the wall?
+   // kill particle
+   
+// if not, have I hit the aggreate (check pixel array)
+   // add to pixel array
+   // put pixel on screen
+   // http://jsperf.com/setting-canvas-pixel
+   
+      //Fill rect!
+
+
+
+
+
+
+
+// hits kill zone
+// hits the aggregate
+//    if hit aggregate add to aggregate
+
+
+
+
+
+
+
+
+
